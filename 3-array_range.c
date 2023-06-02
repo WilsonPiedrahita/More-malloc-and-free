d4cb692583e29b6c4a1034309e626446a8c9c6c7
root <root@DESKTOP-DHALUET>
2023-06-01 19:55:01 -0500

    archivo 3

---
+++ 3-array_range.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *array_range(int min, int max)
+{
+    int *array;
+    int size, i;
+
+    if (min > max)
+        return NULL;
+
+    size = max - min + 1;
+    array = malloc(size * sizeof(int));
+    if (array == NULL)
+        return NULL;
+
+    for (i = 0; i < size; i++)
+        array[i] = min++;
+
+    return array;
+}
